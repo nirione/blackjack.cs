66a2edd627c726d0dd04859ac61d80fb14df1b64
nirione <swierczjakub@yahoo.com>
2022-10-02 23:37:07 +0200

    sync

--- blackjack.cs
+++ blackjack.cs
@@ -6,26 +6,20 @@ using System.Collections.Generic;
 	// each card is a product of figure and color, eg 2 of Spades
 	// each figure has its points: 2-10 respectively, J, Q and K are 10, Ace is either 1 or 11
 	// Hence a class for cards has: Figure, Color, Point
-
-	// deck is composed as an array of cards
-	// deck order has to be randomized each deal
 	
 	// each game has to start by randomizing the deck and then dealing cards player-dealer-player-dealer
-	// each dealt card has to be removed from the deck
-	// player doesnt know dealers hand
 	// possible choices for dealer (randomized): hit, hold
 	// possible choices for player: hit, hold
 	
 	
-namespace BlackJack
+namespace BlackJack{
 
-{
-	class TheGame
-	{
+	class TheGame{
+
 		public static bool init_game = true;
 
-		public static void Main (string[] args)
-		{
+		public static void Main (string[] args){
+
 		    Console.WriteLine("Hello and welcome to Oldman\'s Casino!!");
 			Console.WriteLine();
 			string[] figures = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
@@ -35,27 +29,21 @@ namespace BlackJack
 			List<Card> player_hand = new List<Card>();
 			List<Card> dealer_hand = new List<Card>();
 
-			foreach (string clr in colors)
-			{foreach (string fig in figures)
-					{deck.Add(new Card(fig, clr));}
-			}
+			foreach (string clr in colors) {foreach (string fig in figures ) {deck.Add(new Card(fig, clr)); } }
 			
 			Console.WriteLine("Dealing...");
 			Deal(deck, player_hand, dealer_hand);	
 			while(TheGame.init_game)
-			{ game(deck, player_hand, dealer_hand); }	
+			{ game(deck, player_hand, dealer_hand); } }	
 
+		public static void game (List<Card> aDeck, List<Card> aPlayer_hand, List<Card> aDealer_hand){
 
-		}
-
-		public static void game (List<Card> aDeck, List<Card> aPlayer_hand, List<Card> aDealer_hand)
-		{
 			string user_choice;
 			Console.WriteLine("Pick your choice: 1 - hit | 2 - hold | e - exit");
 			Console.Write(". . . ");
 			user_choice = Console.ReadLine();
-			switch (user_choice)
-			{
+			switch (user_choice) {
+
 				case "1":
 					Console.WriteLine(">>Hitting...");
 					if (aDeck.Count == 0)
@@ -87,8 +75,8 @@ namespace BlackJack
 			}
 		}	
 
-		public static void Hit (List<Card> aDeck, List<Card> aHand)
-		{
+		public static void Hit (List<Card> aDeck, List<Card> aHand) {
+
 			int a = Shuffler(aDeck);
 			int b = aHand.Count;
 
@@ -101,8 +89,8 @@ namespace BlackJack
 			Console.WriteLine("end of hit");
 		}
 		
-		public static void Hold (List<Card> aHand)
-		{
+		public static void Hold (List<Card> aHand) {
+
 			CardsInHand(aHand);
 			Console.WriteLine();
 			Console.WriteLine("Points of hand: {0}", PointCounter(aHand));
@@ -110,8 +98,8 @@ namespace BlackJack
 			
 		}
 
-		public static void Deal (List<Card> aDeck, List<Card> aPlayer_hand, List<Card> aDealer_hand)
-		{
+		public static void Deal (List<Card> aDeck, List<Card> aPlayer_hand, List<Card> aDealer_hand) {
+
 			Console.WriteLine(">>Dealing...");
 			
 			Console.WriteLine("Dealing player...");
@@ -129,14 +117,14 @@ namespace BlackJack
 			Console.WriteLine("end of deal");
 		}
 
-		public static int Shuffler (List<Card> aDeck)
-		{
+		public static int Shuffler (List<Card> aDeck) {
+
 			Random getrand = new Random();
 			return getrand.Next(0, aDeck.Count);
 		}
 
-		public static int PointCounter (List<Card> aHand)
-		{
+		public static int PointCounter (List<Card> aHand) {
+
 			int points, cards;
 			points = 0;
 			cards = aHand.Count - 1;
@@ -146,15 +134,16 @@ namespace BlackJack
 			return points;
 		} 
 
-		public static void CardsInHand (List<Card> aHand)
-		{
+		public static void CardsInHand (List<Card> aHand) {
+
 			List<string> card_names = new List<string>();
 			int cards = aHand.Count;
-			if (aHand.Count != 0)
-			{
+			if (aHand.Count != 0) {
+
 				for (int i = 0; i < cards; i++ )
 				{ card_names.Add(aHand[i].name);}
 			}
+
 			else{ Console.WriteLine("empty hand"); }
 		
 			Console.Write("Cards in hand: ");	
@@ -170,13 +159,13 @@ namespace BlackJack
 
 	}
 
-	class Card
-	{
+	class Card {
+
 		public string figure, color, name;
 		public int point;
 
-		public Card (string aFigure, string aColor)
-		{
+		public Card (string aFigure, string aColor) {
+
 			figure = aFigure;
 			color = aColor;
 			if (aFigure.Length == 1) { point = Convert.ToInt32(aFigure);}
